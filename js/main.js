a80153dacc025a8a9c4821dc0edd6c37677e7389
mistafi <mistafi@gmail.com>
2013-01-17 20:58:03 -0500

    Updating html and js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
 // JavaScript Document
+    	<!-- VFW Project 2 -->
+    	<!-- Joshua Wisecup -->
+    	<!-- Term 0113 -->
+		
+
+
+//Wait until the DOM is ready
+window.addEventListener("DOMContentLoaded", function(){
+
+
+
+
+	//Get ElementById Function
+	function $(x){
+		var theElement = document.getElementById("x");
+		return theElement;
+	}
+	
+	
+	//Create select field element and populate with options
+	function makeCats() {
+		var formTag = document.getElementsByTagName("form"); //formTag is an array
+	}
+	
+	//Variable defaults
+	var pebbleGroups = ["--Choose a Type--", "Restaurant", "Gas Station", "Retail Store"];
+	
+	
+	
+	//Set Link and Submit Click Events
+	var displayLink = $("displayLink");
+	displayLink.addEventListener("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventListener("click", clearLocal);
+	var save = $("submit");
+	save.addEventListener("click", storeData);
+
+
+
+});
+
+
+
+
 
 //var myField = document.getElementById("myText");
 
@@ -8,7 +52,8 @@
 
 //console.log("tags");
 
-var myUlTag = document.createElement("ul");
+
+/*var myUlTag = document.createElement("ul");
 var myDiv = document.getElementById("mainContent");
 
 var colors = [ "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
@@ -22,18 +67,4 @@ for(i=0, j=colors.length; i<j; i++) {
 	liTag.setAttribute = ("class","even");
 	}
 	myUlTag.appendChild(liTag);
-}
-
-
-//Wait until the DOM is ready
-
-
-//Get ElementById Function
-
-
-//Variable defaults
-
-
-//Set Link and Submit Click Events
-
-
+};*/
